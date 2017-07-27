22a150f4c6a48fc7900a721291e62af8a74cfa89
Thibault Person <tperson@siradel.com>
2017-07-27 16:12:53 +0200

    Reply

--- index.js
+++ index.js
@@ -2,19 +2,21 @@ const express = require('express');
 const RtmClient = require('@slack/client').RtmClient;
 const CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
 const RTM_EVENTS = require('@slack/client').RTM_EVENTS;
+const _ = require('lodash');
 const bot_token = process.env.SLACK_BOT_TOKEN || '';
 const rtm = new RtmClient(bot_token);
 const app = express();
 const CHANNELTOUSE = process.env.SLACK_BOT_CHANNEL || 'general'
 
 let channel;
+let self;
+
+let order = {};
 
 // The client will emit an RTM.AUTHENTICATED event on successful connection, with the `rtm.start` payload
 rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, (rtmStartData) => {
-	console.log('Bot connected');
-	console.log(rtmStartData.channels);
+	self = rtmStartData.self;
 	for (const c of rtmStartData.channels) {
-	console.log(c.name);
     if (c.name === CHANNELTOUSE) {
 		channel = c.id
 	}
@@ -27,7 +29,8 @@ rtm.on(CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, function () {
 });
 
 rtm.on(RTM_EVENTS.MESSAGE, function handleRtmMessage(message) {
-  console.log('Message:', message); //this is no doubt the lamest possible message handler, but you get the idea
+	console.log('Message:', message); //this is no doubt the lamest possible message handler, but you get the idea
+	handleMessage(message);
 });
 
 rtm.start();
@@ -37,3 +40,18 @@ app.get('/', function (req, res) {
 });
 
 app.listen(process.env.PORT || 5000);
+
+
+
+function handleMessage(msg) {
+	if (!msg.user) {
+		return;
+	}
+	if (!_.includes(msg.text.match(/<@([A-Z0-9])+>/igm), `<@${this.self.id}>`))
+	{
+		return;
+	}
+	rtm.sendMessage('Merci de penser à moi', msg.channel);
+	
+
+}
