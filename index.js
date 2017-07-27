009aa893eda64edd82667f9ecfa7aecbfc13588b
Thibault Person <tperson@siradel.com>
2017-07-27 14:39:58 +0200

    Add bot boilerplate

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
-var express = require('express');
-var app = express();
+var RtmClient = require('@slack/client').RtmClient;
+var CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
 
-app.set('port', (process.env.PORT || 5000));
+var bot_token = process.env.SLACK_BOT_TOKEN || '';
 
-app.use(express.static(__dirname + '/public'));
+var rtm = new RtmClient(bot_token);
 
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+let channel;
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
+// The client will emit an RTM.AUTHENTICATED event on successful connection, with the `rtm.start` payload
+rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, (rtmStartData) => {
+  for (const c of rtmStartData.channels) {
+	  if (c.is_member && c.name ==='general') { channel = c.id }
+  }
+  console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}, but not yet connected to a channel`);
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+// you need to wait for the client to fully connect before you can send messages
+rtm.on(CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, function () {
+  rtm.sendMessage("Hello!", channel);
 });
+
+rtm.start();
