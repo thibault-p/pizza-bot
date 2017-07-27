52ac3823bed4abe140357d30e423a8e7a7a6aecf
Thibault Person <tperson@siradel.com>
2017-07-27 15:24:05 +0200

    Add missing lib

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var RtmClient = require('@slack/client').RtmClient;
 var CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
-
+var RTM_EVENTS = require('@slack/client').RTM_EVENTS;
 var bot_token = process.env.SLACK_BOT_TOKEN || '';
 
 var rtm = new RtmClient(bot_token);
