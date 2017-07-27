2128c4e4c113bfba81fe61fae7f74005106601f1
Thibault Person <tperson@siradel.com>
2017-07-27 15:08:30 +0200

    Test

--- index.js
+++ index.js
@@ -5,16 +5,24 @@ var bot_token = process.env.SLACK_BOT_TOKEN || '';
 
 var rtm = new RtmClient(bot_token);
 
-let channel = '#general';
+let channel;
 
 // The client will emit an RTM.AUTHENTICATED event on successful connection, with the `rtm.start` payload
 rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, (rtmStartData) => {
 	console.log('Bot connected');
+	for (const c of rtmStartData.channels) {
+	console.log(c_name);
+    if (c.name === 'general') {
+		channel = c.id
+	}
+  }
+
 });
 
 // you need to wait for the client to fully connect before you can send messages
 rtm.on(CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, function () {
-  rtm.sendMessage("Hello!", channel);
+
+	rtm.sendMessage("Hello!", channel);
 });
 
 rtm.start();
