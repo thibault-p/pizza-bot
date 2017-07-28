1b171e2b74d41df82241ac258b5483b187b714fd
Thibault Person <tperson@siradel.com>
2017-07-28 18:05:50 +0200

    Test

--- index.js
+++ index.js
@@ -24,6 +24,24 @@ if (smsService && process.env.OVH_SMS_NOTIFY) {
 	   appKey: process.env.OVH_APP_KEY,
 	   appSecret: process.env.OVH_APP_SECRET
    });
+   ovh.request('POST', '/auth/credential', {
+		'accessRules': [
+		{ 'method': 'GET', 'path': '/*'},
+		{ 'method': 'POST', 'path': '/*'},
+		{ 'method': 'PUT', 'path': '/*'},
+		{ 'method': 'DELETE', 'path': '/*'}
+		]
+	}, function (error, credential) {
+		console.log(error || credential);
+		if (!err) {
+			ovh = require('ovh')({
+			  endpoint: 'ovh-eu',
+			  appKey: process.env.OVH_APP_KEY,
+	   	   	  appSecret: process.env.OVH_APP_SECRET,
+			  consumerKey: credential.consumerKey
+			});
+		}
+	});
 	ovh.request('PUT', '/sms/{serviceName}', {
 		serviceName: smsService,
 		cgiUrl: process.env.OVH_SMS_NOTIFY,
